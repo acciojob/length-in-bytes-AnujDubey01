46e350f9f21372372ba6bd706e734f96b0ae3b9c
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-05-14 18:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	 // Create a Blob from the string
+    const blob = new Blob([str]);
+    // Return the size of the Blob
+    return blob.size;
+	
 };
 
 // Do not change the code below
